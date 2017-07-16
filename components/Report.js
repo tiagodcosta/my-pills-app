b73714ed95bcae429190a180904124fd26779cb8
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-07-16 14:09:11 -0400

    Add iodineAPI

--- components/Report.js
+++ components/Report.js
@@ -4,43 +4,58 @@ import { Icon, Container, Text } from 'native-base'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 
-import { VictoryChart, VictoryBar, VictoryAxis, VictoryPie } from 'victory-native'
+import { VictoryChart, VictoryBar, VictoryAxis } from 'victory-native'
+import Loader from './Loader'
+
+const iodineAPIKey = '5654b09181b03100010000301177b120e8464ddf6ca318b260d87754'
+const iodineAPIUrl = 'https://api.iodine.com/drug/lexapro.json'
 
 class Report extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tips: '',
+      loading: false
+    }
+  }
+
   static navigationOptions = {
       tabBarLabel: 'Report',
       tabBarIcon: ({ tintColor }) =>
         <Icon style={{color: '#44ad8e' }} name="ios-stats" />
   }
 
+
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    })
+    fetch(iodineAPIUrl, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': iodineAPIKey
+      }
+    })
+      .then(response => response.json())
+      .then(drugs => console.log(drugs))
+      .catch(err => console.error('Some error', err))
+  }
+
+  renderLoader() {
+    if(this.state.loading) {
+      return <Loader size="large" />
+    } else {
+      return <Text>{this.state.tips}</Text>
+    }
+  }
+
   render() {
     return (
       <Container>
-      <VictoryChart
-        domainPadding={{x: 40}}
-        >
-            <VictoryBar
-                data={[
-                  { name: "Joan", age: 56},
-                  { name: "Beth", age: 30},
-                  { name: "Bob", age: 45}
-                ]}
-                x="name"
-                y="age"
-            />
-            <VictoryAxis
-                label="name"
-                style={{
-                  axisLabel: { padding: 30 }
-                }}
-              />
-            <VictoryAxis dependentAxis
-                label="Age"
-                style={{
-                  axisLabel: { padding: 40 }
-                }}
-            />
-      </VictoryChart>
+          {this.renderLoader()}
       </Container>
     )
   }
