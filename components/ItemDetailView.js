10c6b7a273b975775eb852ec2c37327dc7401165
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-08-13 17:33:00 -0400

    Fix last commit

--- components/ItemDetailView.js
+++ components/ItemDetailView.js
@@ -5,6 +5,7 @@ import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 import Loader from './Loader'
+import { navigation } from 'react-navigation'
 
 class ItemDetailView extends Component {
   constructor() {
@@ -71,8 +72,8 @@ class ItemDetailView extends Component {
     }
   }
 
-
   render() {
+    const {navigate} = this.props.navigation;
     return(
           <Container>
             <Content showsVerticalScrollIndicator={false}>
@@ -105,6 +106,12 @@ class ItemDetailView extends Component {
                       <Text style={{color: '#8e44ad'}}>Edit</Text>
                     </Button>
                   </Left>
+                  <Body>
+                    <Button transparent onPress={() => navigate('Prescriptions')}>
+                      <Icon style={{color: '#8e44ad', marginRight: 5}} name="arrow-round-back" />
+                      <Text style={{color: '#8e44ad'}}>Back</Text>
+                    </Button>  
+                  </Body>
                   <Right>
                     <Button transparent onPress={() => {this.props.deleteItem(this.props.prescription.uid)}}>
                       <Icon style={{color: '#8e44ad', marginRight: 5}} name="trash" />
